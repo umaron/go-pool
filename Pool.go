11d3fdbaed7c8100ebbcef919646e30ab0735279
SalFumaron <651728079@qq.com>
2021-11-16 15:11:05 +0800

    Initial commit

---
+++ Pool.go
@@ -0,0 +1,23 @@
+package go_pool
+
+type Pool struct {
+	size    int
+	current int
+	channel chan func()
+}
+
+func (p *Pool) Exec(fun func()) {
+	p.channel <- fun
+}
+
+func New(size int) *Pool {
+	p := *&Pool{size: size, channel: make(chan func(), 999999)}
+	for i := 0; i < size; i++ {
+		go func() {
+			for {
+				(<-p.channel)()
+			}
+		}()
+	}
+	return &p
+}
